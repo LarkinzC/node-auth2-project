da619a999df919a0fe5083279e684f14fa5bd149
Larkin Campbell <Larkincampbell24@gmail.com>
2024-03-19 20:57:24 -0700

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -3,6 +3,7 @@ const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 const bcrypt = require('bcryptjs')
 const User = require('../users/users-model')
+const jwt = require('jsonwebtoken')
 
 router.post("/register", validateRoleName, (req, res, next) => {
   /**
